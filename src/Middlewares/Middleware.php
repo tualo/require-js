db1cc0886b403d07ad463a21f2e8593b091cdfcb
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2024-10-21 09:44:30 +0200

    initial

---
+++ src/Middlewares/Middleware.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tualo\Office\RequireJS\Middlewares;
+use Tualo\Office\Basic\TualoApplication;
+use Tualo\Office\Basic\IMiddleware;
+
+class Middleware implements IMiddleware{
+    public static function register(){
+        TualoApplication::use('require-js',function(){
+            try{
+
+                TualoApplication::javascript('require_js', './require-js/require.js',[],-5000000);
+                
+            }catch(\Exception $e){
+                TualoApplication::set('maintanceMode','on');
+                TualoApplication::addError($e->getMessage());
+            }
+        },-100); // should be one of the last
+    }
+}
