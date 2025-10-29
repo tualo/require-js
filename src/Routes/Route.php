e5f5d548beaf54309d00714367e9cf91bfcb05c4
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-29 22:21:35 +0100

    Version bump to 1.1.0

--- src/Routes/Route.php
+++ src/Routes/Route.php
@@ -1,24 +1,26 @@
 <?php
+
 namespace Tualo\Office\RequireJS\Routes;
+
 use Tualo\Office\Basic\TualoApplication;
-use Tualo\Office\Basic\Route As R;
+use Tualo\Office\Basic\Route as R;
 use Tualo\Office\Basic\IRoute;
 
 
-class Route implements IRoute{
-    public static function register(){
+class Route extends \Tualo\Office\Basic\RouteWrapper
+{
+    public static function register()
+    {
 
-        R::add('/require-js/(?P<file>[\/.\w\d\-]+)',function($matches){
-            if (file_exists(dirname(__DIR__,2).'/src/js/'.$matches['file'].'')){
-                $path_parts = pathinfo(dirname(__DIR__,2).'/src/js/'.$matches['file'].'');
-                if ($path_parts['extension']=='js')   TualoApplication::contenttype('application/javascript');
-                if ($path_parts['extension']=='css')   TualoApplication::contenttype('text/css');
-                TualoApplication::etagFile((dirname(__DIR__,2).'/src/js/'.$matches['file'].''));
-            }else{
-                TualoApplication::body("// hm, something is wrong ".$matches['file']);
+        R::add('/require-js/(?P<file>[\/.\w\d\-]+)', function ($matches) {
+            if (file_exists(dirname(__DIR__, 2) . '/src/js/' . $matches['file'] . '')) {
+                $path_parts = pathinfo(dirname(__DIR__, 2) . '/src/js/' . $matches['file'] . '');
+                if ($path_parts['extension'] == 'js')   TualoApplication::contenttype('application/javascript');
+                if ($path_parts['extension'] == 'css')   TualoApplication::contenttype('text/css');
+                TualoApplication::etagFile((dirname(__DIR__, 2) . '/src/js/' . $matches['file'] . ''));
+            } else {
+                TualoApplication::body("// hm, something is wrong " . $matches['file']);
             }
-        },array('get','post'),false);
-
-
+        }, array('get', 'post'), false);
     }
 }
